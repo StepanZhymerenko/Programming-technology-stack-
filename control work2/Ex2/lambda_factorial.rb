52a251f3567430861c3d8b9a23a0151530838dfb
StepanZhymerenko <162688473+StepanZhymerenko@users.noreply.github.com>
2024-11-28 10:21:12 +0100

    Delete control work2 directory

--- control work2/Ex2/lambda_factorial.rb
+++
@@ -1,9 +0,0 @@
-factorial = ->(n) do
-  raise ArgumentError, "Negative numbers are not allowed" if n < 0
-  n == 0 ? 1 : (1..n).inject(1, :*)
-end
-
-# Використання лямбди для чисел від 1 до 5
-(1..5).each do |i|
-  puts "Факторіал #{i} дорівнює #{factorial.call(i)}"
-end
